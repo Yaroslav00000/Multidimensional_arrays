8b6f1c73e248c0b05b8556410a2d48701be546f1
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-05-22 19:14:51 +0300

    add templeted

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <Windows.h>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	srand(time(0));
+
+	return 0;
+}
