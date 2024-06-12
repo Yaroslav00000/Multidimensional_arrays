85b7845dbc88ddddcf36e4fa4cd05c82fc6dff78
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-06-12 20:58:05 +0300

    add templeted

--- main.cpp
+++ main.cpp
@@ -8,7 +8,23 @@ int main()
 {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-	srand(time(0));
-
+	int size;
+	cout << "¬вед≥ть розм≥р масиву: ";
+	cin >> size;
+	int arr[3][3];
+	int num= 1;
+	for (int i = 0; i < size; ++i) {
+		for (int j = 0; j < size; ++j) {
+			arr[i][j] = num;
+			num*=2;
+		}
+	}
+	for (int i = 0; i < size; ++i) {
+		for (int j = 0; j < size; ++j) {
+			cout << arr[i][j] << "\t";
+		}
+		cout << endl;
+	}
+	 
 	return 0;
 }
