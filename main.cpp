070dfab337821d4555f59ab38bb90e8e94a19a2d
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-06-17 21:55:49 +0300

    complited

--- main.cpp
+++ main.cpp
@@ -8,7 +8,20 @@ int main()
 {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-	srand(time(0));
-
-	return 0;
+	int num;
+	cout << "¬вед≥ть число: ";
+	cin >> num;
+	int arr[2][3];
+	for (int i = 0; i < 2; ++i) {
+		for (int j = 0; j < 3; ++j) {
+			arr[i][j] = num;
+			num += 1;
+		}
+	}
+	for (int i = 0; i < 2; ++i) {
+		for (int j = 0; j < 3; ++j) {
+			cout << arr[i][j] << "  ";
+		}
+		cout << endl;
+	}	return 0;
 }
